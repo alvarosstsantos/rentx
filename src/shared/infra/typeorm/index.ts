2b643cf09111e3fe331ba9643a8decf8daf7991f
Alvaro S S T Santos <alvarosstsantos@gmail.com>
2021-04-06 11:00:54 -0300

    added mock database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,15 +1,12 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (): Promise<Connection> => {
-  return createConnection();
+export default async (host = "database_ignite"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database: process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions,
+    })
+  );
 };
-
-// export default async (): Promise<Connection> => {
-//   const defaultOptions = await getConnectionOptions();
-
-//   return createConnection(
-//     Object.assign(defaultOptions, {
-//       host: "database",
-//     })
-//   );
-// };
