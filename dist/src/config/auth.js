44cab0eb0c0128190671683f89ed66e898838244
Alvaro S S T Santos <alvarosstsantos@gmail.com>
2021-04-22 15:10:42 -0300

    Delete dist directory

--- dist/src/config/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secret_token: "39b514340ad3532aadc467e534e5fa37",
-    expires_in_token: "15m",
-    secret_refresh_token: "72b302bf297a228a75730123efef7c41",
-    expires_in_refresh_token: "30d",
-    expires_refresh_token_days: 30,
-};
